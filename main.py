a713125b60f00ffbded3877576499c7a910c12c7
AViiiiii06 <167750050+AViiiiii06@users.noreply.github.com>
2025-05-05 01:27:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # Enables CORS
 
-# Set your Gemini API Key
-genai.configure(api_key="AIzaSyBWhUT8W4Y9YQOKS-Ww8qYcd3R2TYfRbOw")
+GEMINI_API_KEY= os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-pro")
 
 
