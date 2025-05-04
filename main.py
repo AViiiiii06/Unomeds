3a184c61472de188d50b354b698c606ca1c2d71e
AViiiiii06 <167750050+AViiiiii06@users.noreply.github.com>
2025-05-05 01:27:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ CORS(app)  # Enables CORS
 
 GEMINI_API_KEY= os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-pro")
+model = genai.GenerativeModel("gemini-2.5-pro-exp-03-25")
 
 
 @app.route('/')
